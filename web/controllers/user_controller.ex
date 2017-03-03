2a12a00e66d7d7e21742c3a7b91430f39d692280
Eliel A. Gordon <gordoneliel@Eliels-MacBook-Pro.local>
2017-03-03 14:36:48 -0800

    Adds follower and following count for users

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -4,23 +4,26 @@ defmodule Whale2.Api.V1.UserController do
   require IEx
 
   def index(conn, _params) do
-    users = Repo.all(User)
-    user = List.first(users)
-
-#    render(conn, "index.json", users: users)
-
   end
     
   def show(conn, %{"id" => id}) do
     user = Repo.get!(User, id)
-    followers = Repo.all assoc(user, :followers)
-#    follower_count =
-#    IEx.pry
+
+    followers = assoc(user, :followers)
+    followers_count = get_association(followers)
+
+    following = assoc(user, :following)
+    following_count = get_association(following)
+
     conn
-       |> render("show.json", user: user, followers: followers)
+       |> render("show.json", user: user, follower_count: followers_count, following_count: following_count)
   end
 
   def create(conn, _params) do
 
+  end
+  
+  def get_association(assoc) do
+    Repo.one(from u in assoc, select: count(u.id))
   end
 end
--- web/views/api/v1/user_view.ex
+++ web/views/api/v1/user_view.ex
@@ -6,28 +6,14 @@ defmodule Whale2.Api.V1.UserView do
       render_many(users, __MODULE__, "user.json")
     end
 
-    def render("show.json", params = %{user: user, followers: followers}) do
-      render_one(params, __MODULE__, "user.json")
-    end
-
-    def render("user.json", params) do
-    user = params.user.user
-    followers = params.user.followers
-
-      %{id: user.id,
-        name: user.first_name <> " " <> user.last_name,
-        username: user.username,
-        image_url: Whale2.Uploaders.UserImage.url({user.image_url, user}, :thumb),
-        email: user.email,
-        followers: render_many(followers, __MODULE__, "relationship.json")
+    def render("show.json", params = %{user: user, follower_count: follower_count, following_count: following_count}) do
+        %{  id: user.id,
+            name: user.first_name <> " " <> user.last_name,
+            username: user.username,
+            image_url: Whale2.Uploaders.UserImage.url({user.image_url, user}, :thumb),
+            email: user.email,
+            follower_count: follower_count,
+            following_count: following_count
         }
-    end
-    
-    def render("relationship.json", relationship) do
-      %{id: relationship.user.id,
-        username: relationship.user.username,
-        email: relationship.user.email,
-        image_url: Whale2.Uploaders.UserImage.url({relationship.user.image_url, relationship.user}, :thumb)
-      }
     end
 end
