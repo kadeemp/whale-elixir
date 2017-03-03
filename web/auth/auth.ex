98d1d9f5568e22d055cce5f2d7540a2c25cbfbbc
Chase Wang <hi@ocwang.com>
2017-03-03 15:40:45 -0800

    finished guardian auth

--- web/auth/auth.ex
+++ web/auth/auth.ex
@@ -1,6 +1,12 @@
 defmodule Whale2.Auth do
   import Plug.Conn
 
+  def init(opts), do: opts
+  def call(conn, _opts) do
+    current_user = Guardian.Plug.current_resource(conn)
+    assign(conn, :current_user, current_user)
+  end
+
   def login(conn, user) do
     new_conn = Guardian.Plug.api_sign_in(conn, user)
     jwt = Guardian.Plug.current_token(new_conn)
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -16,28 +16,19 @@ defmodule Whale2.Api.V1.UserController do
     following_count = get_association(following)
 
     conn
-       |> render("show.json", user: user, follower_count: followers_count, following_count: following_count)
+    |> render("show.json", user: user, follower_count: followers_count, following_count: following_count)
   end
 
   def create(conn, params) do
-    # changeset = User.registration_changeset(%User{}, user_params)
-    # case Repo.insert(changeset) do
-    #   {:ok, user} ->
-    #     conn
-    #     |> Rumbl.Auth.login(user)
-    #     |> put_flash(:info, "#{user.name} created!")
-    #     |> redirect(to: user_path(conn, :index))
-    #   {:error, changeset} ->
-    #     render(conn, "new.html", changeset: changeset)
-    # end
-
     changeset = User.changeset(%User{}, params)
+
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
         |> Whale2.Auth.login(user)
         |> put_status(:created)
-        |> render("show.json", user: user)
+        # TODO: remove hard coded follower/following count
+        |> render("show.json", user: user, follower_count: 0, following_count: 0)
       {:error, _changeset} ->
         send_resp(conn, 422, "")
     end
